540b8d5a082c1719994ff3e2fc111381acd3d706
nany-d <mdanny@hotmail.co.uk>
2021-09-21 09:30:04 +0100

    GROUP-46 spring backend

---
+++ Group3-Backend-Spring/src/test/java/com/group3/Group3BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
