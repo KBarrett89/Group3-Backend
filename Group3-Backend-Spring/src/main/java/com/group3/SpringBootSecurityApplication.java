a053aaf8a926a4930a1914f9d7b6ec697e30226a
RyanCD1 <ryandonaghue0@gmail.com>
2021-09-23 11:49:38 +0100

    GROUP-32 attempting to make main method locatable by Jenkins

--- Group3-Backend-Spring/src/main/java/com/group3/SpringBootSecurityApplication.java
+++ Group3-Backend-Spring/src/main/java/com/group3/SpringBootSecurityApplication.java
@@ -1,4 +1,4 @@
-package com.javainuse.springbootsecurity;
+package com.group3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
