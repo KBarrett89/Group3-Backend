c5e769b67033f5062990be62dc9ca1676efde959
nany-d <mdanny@hotmail.co.uk>
2021-09-28 10:01:26 +0100

    GROUP-110 fix column names

--- sqlScript.sql
+++ sqlScript.sql
@@ -1,5 +1,5 @@
 insert into `vehicle_registration` 
-	(`registrationid`, `registration_date`, `vehicle_registrationno`, `car_make`, `car_model`, `car_colour`, `forename`, `surname`, `address`, `date_of_birth`, `driver_licenceid`) 
+	(`registrationid`, `registration_date`, `vehicle_registrationno`, `make`, `model`, `colour`, `forename`, `surname`, `address`, `date_of_birth`, `driver_licenceid`) 
 values 
 	('1', '2000-01-01', '1QW1 234', 'Audi', 'A5', 'Black', 'Jon', 'Smith', '13 Baker Street', '2002-01-01', '300201');
 
