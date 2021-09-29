a264a110b073e7a6e0616bb654b4c3924a62dbfb
RyanCD1 <donaghueryan0@gmail.com>
2021-09-29 19:41:11 +0100

    GROUP-105 GROUP-33 added java backend for hadnling auth through sql user table and for registering new user if given admin permissions

--- Backend-Demo/src/main/java/com/group3/controller/AuthenticationController.java
+++ Backend-Demo/src/main/java/com/group3/controller/AuthenticationController.java
@@ -27,6 +27,7 @@ import com.group3.config.CustomUserDetailsService;
 import com.group3.config.JwtUtil;
 import com.group3.model.AuthenticationRequest;
 import com.group3.model.AuthenticationResponse;
+import com.group3.model.UserDTO;
 
 import io.jsonwebtoken.impl.DefaultClaims;
 
@@ -84,5 +85,10 @@ public class AuthenticationController {
 			new SecurityContextLogoutHandler().logout(request, response, auth);
 		}
 		return "redirect:/login?logout";
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
+		return ResponseEntity.ok(userDetailsService.save(user));
 	}
 }
